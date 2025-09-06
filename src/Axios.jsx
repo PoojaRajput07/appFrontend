89ef4747f24971379d1241eb3677b5b08e09de3a
PoojaRajput07 <007poojarajput@gmail.com>
2025-09-06 18:48:24 +0530

    axios improved

--- src/Axios.jsx
+++ src/Axios.jsx
@@ -1,9 +1,9 @@
 import axios from "axios"
-// const BASE_URL =
-//   import.meta.env.MODE === "development"
-//     ? "http://localhost:5000/api"   // local testing ke liye
-//     : "https://appbackend-ahuc.onrender.com/api";  // Render backend URL
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000/api"   // local testing ke liye
+    : "https://appbackend-ahuc.onrender.com/api";  // Render backend URL
+// const BASE_URL = "http://localhost:5000/api";
 
 
 const api=axios.create({
